b8a20e4bf7b9e75113459ab210c8cfef99c5d4ef
RundongGao <asphinx423@gmail.com>
2018-05-20 18:05:30 -0400

    creating a result data type

--- test.scala
+++ test.scala
@@ -27,13 +27,26 @@
 //val failingProp = forAll(intList)(ns => ns.reverse == ns)
 
 
-//def listOfN[A](n: Int, a: Gen[A]): Gen[List[A]]
-//def forAll[A](a: Gen[A])(f: A => Boolean): Prop
-
-//trait Prop { def check: Either[(FailedCase, SuccessCount), SuccessCount] }
 
 import fpinscala.state._
 
+//type TestCases = Int
+//type Result = Option[(FailedCase, SuccessCount)]
+//case class Prop(run: TestCases => Result)
+
+sealed trait Result {
+	def isFalsified: Boolean
+}
+type SuccessCount = Int
+type FailedCase = String
+
+case object Passed extends Result {
+	def isFalsified = false
+}
+case class isFalsified(failure: FailedCase,
+                       success: SuccessCount) extends Result {
+	def isFalsified = true
+}
 
 object Prop {
 	type FailedCase = String
@@ -41,9 +54,23 @@ object Prop {
 }
 
 case class Gen[A](sample: State[RNG, A]){
+	def flatMap[B](f: A => Gen[B]): Gen[B] = 
+		Gen(sample.flatMap(a => f(a).sample))
+	def listOfN(n: Int): Gen[List[A]] =
+		Gen.listOfN(n, Gen(sample))
+	def listOfN(size: Gen[Int]): Gen[List[A]] =
+		size flatMap(n => this.listOfN(n))
 }
+
 object Gen {
 	def unit[A](a: => A): Gen[A] = Gen(State.unit(a))
+	def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+		Gen.boolean.flatMap( b => if (b) g1 else g2)
+	def weight[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
+		val ratio = g1._2 / (g1._2 + g2._2)
+		Gen(State(RNG.double).flatMap(random => if (random < ratio) g1._1.sample else g2._1.sample))
+	}
+
 	def boolean: Gen[Boolean] = Gen(State(RNG.nonNegativeInt).map(n => n % 2 == 0))
 	def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
 		Gen(State.sequence(List.fill(n)(g.sample)))
